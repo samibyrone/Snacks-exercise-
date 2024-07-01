cd59bbc1b4a23b8323c606f22bbd27e5757f3974
Samson Ibironke <samsonibironke2@gmail.com>
2024-07-01 06:54:15 +0100

    Add files via upload

---
+++ PersonalityTest.java
@@ -0,0 +1,157 @@
+import java.util.Scanner;
+
+public class PersonalityTest {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+			char select = 'A';
+			char collect = 'B';
+			
+	
+			System.out.println();
+			System.out.println(" You are Welcome to Personality Test Portal ");
+			System.out.println();
+
+       			System.out.println("""
+                			\n
+                			MBTI Personality Test
+                			The following test is designed to measure your MBTI Personality type.\s
+                			Several hints about how to best complete this survey:
+                			There are no right answers to any of these questions.
+                			Answer the questions quickly, do not over-analyze them. Some seem worded poorly. Go with what feels best.
+                			Answer the questions as “the way you are”, not “the way you’d like to be seen by others”
+                			Do not look at the scoring sheet until you have completed all the questions.\s
+                			""");
+					
+			System.out.println("""
+                			\n
+					The INFP Personality Type INFPs are imaginative idealists, \s
+					guided by their one core values and beliefs. to the Healer, possibilities are paramount;
+					the realism of the moment is only of passing concern. 
+					They see potential for a better future, and pursue truth and meaning with their own individual flair. \s
+					""");
+
+					
+
+
+			System.out.print(" what is your name: ");
+			String name = input.next();
+			System.out.println();
+
+			System.out.printf("%n Hello %s you can now make your selection." , name);
+			System.out.println();
+
+			System.out.println();
+			System.out.println(" Select your answer with either A or B untill the end of the assessment ");
+			System.out.println();
+
+			System.out.println(" Kindly enter YES to continue and NO to exit ");
+			name = input.next();
+
+			System.out.println( " A. Expend energy, enjoy gorups			B. Conserve energy, enjoy one-on-one ");
+			System.out.println();
+
+			System.out.println( " A. Interpret literally			B. Look for meaning and possibilities ");
+			System.out.println();
+
+			System.out.println( " A. Logical, thinking, questioning			B. Empathetic, feeling, accomodating ");
+			System.out.println();
+
+			System.out.println( " A. Organized, orderly			B. Flexible, adaptable ");
+			System.out.println();
+
+			System.out.println( " A. More outgoing, think out loud			B. More reseverd, think you yourself ");
+			System.out.println();
+
+			System.out.println( " A. Pratical, realistic, experiential			B. Imaginative, innovative, theoretical ");
+			System.out.println();
+	
+			System.out.println( " A. Candid, straight forward, frank			B. tactful, kind, encouraging ");
+			System.out.println();
+	
+			System.out.println( " A. Plan, schedule			B. Unplanned, spontaneous ");
+			System.out.println();
+	
+			System.out.println( " A. Seek many tasks, public activities, interaction with others			B. Seek private, solitary activities with quiet to concentrate ");
+			System.out.println();
+	
+			System.out.println( " A. Standard, usual, conventional			B. Diffrent, novel, unique ");
+			System.out.println();
+	
+			System.out.println( " A. Firm, tend to criticize, hold the line			B. Gentle, tend to appreciate, conciliate ");
+			System.out.println();
+	
+			System.out.println( " A. Regulated, structured			B. Easy-going, live and let live ");
+			System.out.println();
+	
+			System.out.println( " A. External, communicative, express yourself			B. Internal, reticent, keep to yourself ");
+			System.out.println();
+	
+			System.out.println( " A. Focus on here-and-now			B. Loook to the future, global perspective, big picture ");
+			System.out.println();
+	
+			System.out.println( " A. Tough-minded, just			B. Tender-hearted, merciful ");
+			System.out.println();
+	
+			System.out.println( " A. Preparation, plan ahead			B. Go with the flow, adapt as you go ");
+			System.out.println();
+	
+			System.out.println( " A. Active, initiate			B. Reflective, delibrate ");
+			System.out.println();
+	
+			System.out.println( " A. Facts, things, what is			B. Ideas, dreams, what could be, philosophical ");
+			System.out.println();
+	
+			System.out.println( " A. Matter of fact, issue-oriented			B. Sensitive, people-oriented, compassionate ");
+			System.out.println();
+	
+			System.out.println( " A. Control, govern			B. Latitude, freedom ");
+			System.out.println();
+	
+			System.out.println(" A. candid, straight forward, frank		B. tactful, kind, encouraging ");
+			System.out.println();
+	
+			System.out.println(" A. candid, straight forward, frank		B. tactful,kind, encouraging ");
+			System.out.println();
+	
+			System.out.println(" A. candid, straight forward, frank		B. tactful, kind, encouraging ");
+			System.out.println();
+	
+			System.out.println(" A. candid, straight forward, frank		B. tactful, kind, encouraging ");
+			System.out.println();
+	
+			System.out.println(" A. candid, straight forward, frank		B. tactful, kind, encouraging ");
+			System.out.println();
+	
+					
+						//	int select = input.nextInt();
+
+
+//			Number of A selected:
+//			Number of B selected:
+
+//			Extroverted E	Vs	Introverted I :
+//			Sensing S	Vs	Intuitive N :
+//			Thinking T	Vs	Feeling F :
+//			Judging J	Vs	Perceptive :
+
+
+//	Expected A or B as response
+//	I know this is an error, please retry again
+
+//	INFPs are sensitive, caring, and compassionate, and are deeply conserned with the personal growth of themselves and others. Individualistic and nonjudgmental, INFPs believe that each person must find their own path. They enjoy //	spending time exploring their own ideas and values, and are gently encoraging to others to do the same. INFPs are creative and often artistic; they enjoy finding new outlets for self-expression.
+
+//	what does INFP stand for?
+//	INFP is one of the sixteen personality types created by Katherine Briggs and Isabel Myers, creators of the Myers-Briggs Types Indicator (MBTI). INFP stands for Introversion, Intuition, Feeling, and Percieving, 
+//	which are four core personality traits based on the work of psychologist C.G. jung.
+
+//	Each of the four letters of the INFP code signifies a key personality traits of this type. INFPs are energized by time alone (Intorverted), focus on ideas and concepts rather than facts and details (Intuitive), 
+//	make decisions	based on feelings and values (Feeling), and prefer to be spontaneous and flexible rather than planned and organized (Perceiving).
+		
+	}
+
+}
+
+
